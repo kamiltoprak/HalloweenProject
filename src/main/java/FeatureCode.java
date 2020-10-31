906673181754f91a965d98be9ec15c7088447d25
kamiltoprak <ktoprak@gmail.com>
2020-10-31 14:39:52 -0400

    nineth local commit on test java

--- src/main/java/FeatureCode.java
+++ src/main/java/FeatureCode.java
@@ -4,5 +4,8 @@ public class FeatureCode {
 
     // what  else can  we do
 
+    public static void main(String[] args) {
+        System.out.println("avesome  feature is in progress ");
+    }
 
 }
